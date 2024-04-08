39be7dd9cafbc24b2120831bf62ade45f0e1515a
SaamiiR24 <samir.ahouch@alumnos.upm.es>
2024-04-08 10:32:34 +0200

    movimientos

--- lib/Piezas/alfil.h
+++ lib/Piezas/alfil.h
@@ -15,18 +15,24 @@ void alfil::updatePosPosibles(Tablero tab)
 		if (tab.mat[i][j] == 0) {
 			// Si la posición es válida, pone esa posición en la matris de posicones válidas a 1
 			posiciones_validas[i][j] = true;
+			tab.printPosibles(x, y);
+
 		}
 	}
 
 	for (int i = x + 1, int j = y - 1; tab.dim_x && tab.dim_y; i++, j--) {
 		if (tab.mat[i][j] == 0) {
 			posiciones_validas[i][j] = true;
+			tab.printPosibles(x, y);
+
 		}
 	}
 
 	for (int i = x - 1, int j = y + 1; tab.dim_x && tab.dim_y; i--, j++) {
 		if (tab.mat[i][j] == 0) {
 			posiciones_validas[i][j] = true;
+			tab.printPosibles(x, y);
+
 		}
 	}
 
@@ -34,6 +40,8 @@ void alfil::updatePosPosibles(Tablero tab)
 
 		if (tab.mat[i][j] == 0) {
 			posiciones_validas[i][j] = true;
+			tab.printPosibles(x, y);
+
 		}
 	}
 }
--- lib/Piezas/peon.h
+++ lib/Piezas/peon.h
@@ -1,28 +1,26 @@
 #include "pieza.h"
-#include "tablero.h"
 
 class peon : pieza
 {
 public:
 
-	int x, y; //pos inicial del peon
-	int** tab;
-	int dim_x, dim_y;
 	int color;//1 blancas, -1 negras
 
-	void updatePosPosibles(Tablero Tablero) {
+	void updatePosPosibles(Tablero tab) {
 
 		int direccion = (color == 1) ? 1 : -1;
 
 		//comprueba si es la fila inicial
 
-		if (color == 1 && y == 1 || color == -1 && dim_y == 6) {
+		if (color == 1 && y == 1 || color == -1 && tab.dim_y == 6) {
 			if (y + (direccion * 2) == 0) {
-					Tablero::printPosibles(x, y);
+					tab.printPosibles(x, y);
+					posiciones_validas[x][y] = true;
 				}
 			
 		}else if (y + direccion == 0) {
-			Tablero::printPosible(x, y);
+			tab.printPosibles(x, y);
+			posiciones_validas[x][y] = true;
 		}
 
 	}
