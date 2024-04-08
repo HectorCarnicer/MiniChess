3ba20dfe803a6a2388ae5241e18b105f1feaa2f1
SaamiiR24 <samir.ahouch@alumnos.upm.es>
2024-04-08 13:10:24 +0200

    modificación de alfil para que solo detecte la primera pieza enemiga
    solo detecta como movimiento valido hasta la primera pieza enemiga de una diagonal

--- lib/Piezas/alfil.h
+++ lib/Piezas/alfil.h
@@ -1,47 +1,65 @@
-#include "pieza.h"
+#include "tablero.h"
+#include <cstdlib>
 
-class alfil : pieza
+class alfil
 {
 public:
+    void updatePosPosibles(Tablero& tablero) {
+        int x = 4, y = 4; // Posición inicial del alfil
+        int cont = 1;
+        bool foundEnemy = false; // Bandera para seguir si se ha encontrado una pieza enemiga
 
-	// Función que encuentra las posiciones específicas para el alfil
-	void updatePosPosibles(Tablero tab);
+        // Recorre el tablero en las diagonales
+        for (int i = x + 1, j = y + 1; i < 8 && j < 8; i++, j++) {
+
+            if (tablero.mat[i][j] == 0) {
+                tablero.printPosibles(i, j);
+                system("cls");
+            }
+
+            else if (tablero.mat[i][j] < 0 && !foundEnemy) {
+                tablero.mat[i][j] = 9; // Cambia la primera pieza enemiga encontrada a '9'
+                foundEnemy = true; // Actualiza la bandera para indicar que se encontró una pieza enemiga
+            }
+        }
+        foundEnemy = false;
+
+        for (int i = x + 1, j = y - 1; i < 8 && j >= 0; i++, j--) {
+            if (tablero.mat[i][j] == 0) {
+                tablero.printPosibles(i, j);
+                system("cls");
+            }
+            else if (tablero.mat[i][j] < 0 && !foundEnemy) {
+                tablero.mat[i][j] = 9; // Cambia la primera pieza enemiga encontrada a '9'
+                foundEnemy = true; // Actualiza la bandera para indicar que se encontró una pieza enemiga
+            }
+
+        }
+        foundEnemy = false;
+        for (int i = x - 1, j = y + 1; i >= 0 && j < 8; i--, j++) {
+            if (tablero.mat[i][j] == 0) {
+
+                tablero.printPosibles(i, j);
+                system("cls");
+            }
+            else if (tablero.mat[i][j] < 0 && !foundEnemy) {
+                tablero.mat[i][j] = 9; // Cambia la primera pieza enemiga encontrada a '9'
+                foundEnemy = true; // Actualiza la bandera para indicar que se encontró una pieza enemiga
+            }
+        }
+        foundEnemy = false;
+        for (int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--) {
+            if (tablero.mat[i][j] == 0) {
+
+                tablero.printPosibles(i, j);
+                system("cls");
+            }
+            else if (tablero.mat[i][j] < 0 && !foundEnemy) {
+                tablero.mat[i][j] = 9; // Cambia la primera pieza enemiga encontrada a '9'
+                foundEnemy = true; // Actualiza la bandera para indicar que se encontró una pieza enemiga
+            }
+
+        }
+    }
+
 };
-
-void alfil::updatePosPosibles(Tablero tab)
-{
-	//recorre el tablero
-	for (int i = x + 1, int j = y + 1; tab.dim_x && tab.dim_y; i++, j++) {
-		if (tab.mat[i][j] == 0) {
-			// Si la posición es válida, pone esa posición en la matris de posicones válidas a 1
-			posiciones_validas[i][j] = true;
-			tab.printPosibles(x, y);
-
-		}
-	}
-
-	for (int i = x + 1, int j = y - 1; tab.dim_x && tab.dim_y; i++, j--) {
-		if (tab.mat[i][j] == 0) {
-			posiciones_validas[i][j] = true;
-			tab.printPosibles(x, y);
-
-		}
-	}
-
-	for (int i = x - 1, int j = y + 1; tab.dim_x && tab.dim_y; i--, j++) {
-		if (tab.mat[i][j] == 0) {
-			posiciones_validas[i][j] = true;
-			tab.printPosibles(x, y);
-
-		}
-	}
-
-	for (int i = x - 1, int j = y - 1; tab.dim_x && tab.dim_y; i--, j--) {
-
-		if (tab.mat[i][j] == 0) {
-			posiciones_validas[i][j] = true;
-			tab.printPosibles(x, y);
-
-		}
-	}
-}
