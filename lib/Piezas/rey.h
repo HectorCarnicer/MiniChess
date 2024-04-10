18187ab57a727836910b3c437a31f400dfb16c50
SaamiiR24 <samir.ahouch@alumnos.upm.es>
2024-04-10 11:16:11 +0200

    mejoras del rey

--- lib/Piezas/rey.h
+++ lib/Piezas/rey.h
@@ -9,25 +9,28 @@ public:
 
 };
 
-void rey::updatePosPosibles(Tablero tab){
-		int movimientospos[8][2] = { {1,0}, {1,1}, {1,-1}, {0,1}
-								,{0,-1}, {-1,0}, {-1,1}, {-1,-1} };
+void rey::updatePosPosibles(Tablero tab) {
 
-		for (int i = 0; i < 8; i++) {
-			//recorre las L del caballo posibles
-			int posposiblex = x + movimientospos[i][0];
-			int posposibley = y + movimientospos[i][1];
-			
-			if (posposiblex >= 0 && posposiblex < tab.dim_x && posposibley >= 0 && posposibley < dim_y) {
-				//comprueba que este vacía
-				if (tab.defTablero[posposiblex][posposibley] == 0) {				
-					tab.printPosibles(posposiblex, posposibley);
-					posiciones_validas[posposiblex][posposibley] = true;
+	int y = 2, x = 2;
+	int id = REY_B;
+	int movimientospos[8][2] = { {1,0}, {1,1}, {1,-1}, {0,1}
+							,{0,-1}, {-1,0}, {-1,1}, {-1,-1} };
 
-					}
+	for (int i = 0; i < 8; i++) {
+		//recorre las L del caballo posibles
+		int posposiblex = x + movimientospos[i][0];
+		int posposibley = y + movimientospos[i][1];
+
+		if (posposiblex >= 0 && posposiblex < tab.dim_x && posposibley >= 0 && posposibley < tab.dim_y) {
+			//comprueba que este vacía
+			if (tab.defTablero[posposiblex][posposibley] <= 0) {
+				tab.printPosibles(posposiblex, posposibley, x ,y, REY_B);
 			}
-
+			
 		}
 
 	}
+}
+
+
 
