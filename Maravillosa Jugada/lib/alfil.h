6b681186307a7de9e17418e6fc0214f1fae05db4
SaamiiR24 <samir.ahouch@alumnos.upm.es>
2024-05-25 18:58:43 +0200

    .

--- Maravillosa Jugada/lib/alfil.h
+++ Maravillosa Jugada/lib/alfil.h
@@ -1,1 +1,35 @@
-#pragma once
+ 
+#ifndef ALFIL_H
+#define ALFIL_H
+
+
+#include "pieza.h"
+
+class Alfil : public Pieza
+{
+public:
+
+	Alfil(int posX, int posY, Color color, int TAM_TAB) : Pieza(posX, posY, color, TAM_TAB) {}
+
+    bool  mover(int x, int y)override {
+        // Recorre el tablero en las diagonales
+        for (int i = x + 1, j = y + 1; i < 8 && j < 8; i++, j++) {
+
+        }
+
+        for (int i = x + 1, j = y - 1; i < 8 && j >= 0; i++, j--) {
+
+        }
+
+        for (int i = x - 1, j = y + 1; i >= 0 && j < 8; i--, j++) {
+
+        }
+
+        for (int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--) {
+        }
+    }
+};
+
+
+
+#endif
