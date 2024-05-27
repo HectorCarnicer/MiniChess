80d882bc6da4c5dac43bbc41d478ad7e853dc641
OscarFelipeGonzalez <159053904+OscarFelipeGonzalez@users.noreply.github.com>
2024-05-27 02:03:25 +0200

    Preparación para inclusión

--- Maravillosa Jugada/src/main.cpp
+++ Maravillosa Jugada/src/main.cpp
@@ -1,38 +1,38 @@
-#include <iostream>
-#include <vector>
-#include "../lib/Pieza.h"
-#include "../lib/Peon.h"
-#include "../lib/Rey.h"
-#include "../lib/gardner.h"
+//#include <iostream>
+//#include <vector>
+//#include "../lib/Pieza.h"
+//#include "../lib/Peon.h"
+//#include "../lib/Rey.h"
+//#include "../lib/gardner.h"
+//
+//
+//
+//
+//int main() {
+//    // Crear un vector para almacenar las pieza
+//
+//    std::vector<Pieza*> piezas;
+//    Gardner gardner(piezas);
+//    gardner.inicializa();
+//    //Primer turno es blanco
+//    Color turnoActual = BLANCO; // Comenzar con el turno de las piezas blancas
+//
+//    // Bucle que genera nuevas jugadas
+//    while (true) {
+//        gardner.imprimirTablero();
+//        gardner.nuevaJugada(turnoActual);
+//    }
+//    return 0;
+//}
 
 
 
 
-int main() {
-    // Crear un vector para almacenar las pieza
-
-    std::vector<Pieza*> piezas;
-    Gardner gardner(piezas);
-    gardner.inicializa();
-    //Primer turno es blanco
-    Color turnoActual = BLANCO; // Comenzar con el turno de las piezas blancas
-
-    // Bucle que genera nuevas jugadas
-    while (true) {
-        gardner.imprimirTablero();
-        gardner.nuevaJugada(turnoActual);
-    }
-    return 0;
 
 
 
 
 
 
-
-
-
-
-}
 
     
